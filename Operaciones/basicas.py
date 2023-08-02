18c10d9f0ea5c12aa36afca71519bfa512a6f6ac
BrunoArroyo <bruno@itq.edu.ec>
2023-08-01 19:11:23 -0500

    funcion multiplicacion

--- Operaciones/basicas.py
+++ Operaciones/basicas.py
@@ -7,9 +7,11 @@ def main():
         # Realizar la suma
         suma = numero1 + numero2
         resta= numero1 - numero2
+        multi= numero1 * numero2
         # Mostrar el resultado
         print("La suma de {} y {} es: {}".format(numero1, numero2, suma))
         print("La resta de {} y {} es: {}".format(numero1, numero2, resta))
+        print("La multiplicacion de {} y {} es: {}".format(numero1, numero2, multi))
     except ValueError:
         print("Error: Ingresa solo números válidos.")
 
